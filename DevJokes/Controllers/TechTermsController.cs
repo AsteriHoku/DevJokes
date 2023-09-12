2d851a41a000c61cb23aa1387f8c86071eedb519
PhoebeHoku <95447747+PhoebeHoku@users.noreply.github.com>
2023-09-12 18:06:27 -0400

    Beginning of Tech Terms

---
+++ DevJokes/Controllers/TechTermsController.cs
@@ -0,0 +1,19 @@
+﻿using DevJokes.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevJokes.Controllers;
+
+public class TechTermsController : Controller
+{
+    public TechTermsController()
+    {
+        
+    }
+
+    [Route("/TechTerms")]
+    public async Task<IActionResult> TechTerms()
+    {
+        var techVM = new TechTermVM();
+        return View(techVM);
+    }
+}
---
+++ DevJokes/ViewModels/TechTermVM.cs
@@ -0,0 +1,8 @@
+﻿namespace DevJokes.ViewModels;
+
+public class TechTermVM
+{
+    public string Name { get; set; }
+    public string Meaning1 { get; set; }
+    public string Meaning2 { get; set; }
+}
